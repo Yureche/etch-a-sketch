05349b86ab2a2cf1d687ac3f4d0943c87fc80c22
Yureche <yurigerber.g18@gmail.com>
2021-08-31 15:51:51 +0300

    Improved code readabilty and added function to create new grid

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Define items 
 const container = document.getElementById("container");
+const clearBtn = document.querySelector('#clear');
+const newGridBtn = document.querySelector("#newGrid");
 
-function makeRows(rows, cols) {
+// Functions
+function createGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
@@ -10,19 +14,46 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(16, 16);
-
-// const gridItems = document.querySelectorAll('.grid-item');
-// gridItems.forEach(cell => cell.addEventListener('mouseeenter', () =>{cell.classList.add('hover')}))
-const gridItems = document.querySelectorAll('.grid-item')
-document.querySelectorAll('.grid-item').forEach(cell => {
+function colorCells() {
+  document.querySelectorAll('.grid-item').forEach(cell => {
     cell.addEventListener('mouseenter', ()=>{cell.classList.add('hover')})
 })
+}
 
 function erase() {
+  const gridItems = document.querySelectorAll('.grid-item')
   gridItems.forEach(cell => {cell.classList.remove('hover')})
 }
-btn = document.querySelector('#clear');
-btn.addEventListener('click',() => {
+
+function removeGrid() {
+  document.querySelectorAll('.grid-item').forEach(item => {item.remove()})
+}
+
+function createNewGrid() {
+  rows = parseInt( prompt("How many rows do you want? ", 16))
+  cols = parseInt(prompt("How many cols do you want? ", 16) )
+  removeGrid()
+  createGrid(rows,cols)
+  colorCells()
+}
+
+// Events
+clearBtn.addEventListener('click',() => {
   erase()
 })
+
+
+newGridBtn.addEventListener('click',() => {
+  createNewGrid()
+})
+
+// Call functions
+createGrid(16, 16);
+colorCells()
+
+
+
+
+
+
+
